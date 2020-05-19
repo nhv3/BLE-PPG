4a9a7c1a67d2d98dbbedc66092e372d3d1e6ef48
Nicholas Vitale <nicholasvitale@Nicholass-MacBook-Pro.local>
2020-05-18 17:53:35 -0700

    First commit

---
+++ pca10040/s132/ses/Output/nrf52-ble-tutorial-characteristic Debug/Obj/Calibration_AFE4404.d
@@ -0,0 +1,4 @@
+Output/nrf52-ble-tutorial-characteristic\ Debug/Obj/Calibration_AFE4404.o: \
+ /Nordic/nRF5_SDK_15.0.0_a53641a/examples/peripheral/wearS-v1-2019-08-15-multi-oects-demo/Calibration_AFE4404.c \
+ /Nordic/nRF5_SDK_15.0.0_a53641a/examples/peripheral/wearS-v1-2019-08-15-multi-oects-demo/Calibration_AFE4404.h \
+ /Nordic/nRF5_SDK_15.0.0_a53641a/examples/peripheral/wearS-v1-2019-08-15-multi-oects-demo/AFE4404.h
